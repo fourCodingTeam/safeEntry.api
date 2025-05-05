8b96774d017a1d15353485d6735ff291bfbca440
taigakilla <taigakilla@gmail.com>
2025-05-05 01:14:31 -0300

    create entities

---
+++ src/SafeEntry.Domain/Entities/Address.cs
@@ -0,0 +1,26 @@
+﻿namespace SafeEntry.Domain.Entities;
+
+public class Address
+{
+    public int Id { get; protected set; }
+    public string Street { get; protected set; }
+    public int Number { get; protected set; }
+    public string District { get; protected set; }
+    public string ZipCode { get; protected set; }
+    public string City { get; protected set; }
+    public string State { get; protected set; }
+    public string Country { get; protected set; }
+
+    protected Address() { }
+
+    public Address(string street, int number, string district, string zipCode, string city, string state, string country)
+    {
+        Street = street ?? throw new ArgumentNullException(nameof(street));
+        Number = number;
+        District = district ?? throw new ArgumentNullException(nameof(district));
+        ZipCode = zipCode ?? throw new ArgumentNullException(nameof(zipCode));
+        City = city ?? throw new ArgumentNullException(nameof(city));
+        State = state ?? throw new ArgumentNullException(nameof(state));
+        Country = country ?? throw new ArgumentNullException(nameof(country));
+    }
+}
---
+++ src/SafeEntry.Domain/Entities/Resident.cs
@@ -0,0 +1,13 @@
+﻿namespace SafeEntry.Domain.Entities;
+
+public class Resident : Person
+{
+    public Address address { get; protected set; }
+
+    public Resident(string name, long cpf, long phoneNumber, Address address)
+        : base(name, cpf, phoneNumber)
+    {
+        Address = address ?? throw new ArgumentNullException(nameof(address));
+    }
+
+}
